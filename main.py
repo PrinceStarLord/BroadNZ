ee95f0be2c6d8f89927581385d7d06f27ace4715
Tushar Patil <86295965+PrinceStarLord@users.noreply.github.com>
2023-02-02 09:44:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -45,9 +45,6 @@ async def startprivate(client, message):
             )
         else:
             logging.info(f"#NewUser :- Name : {message.from_user.first_name} ID : {message.from_user.id}")
-    
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\nI'm a simple Telegram bot that can broadcast messages and media to the bot subscribers"
-    await message.reply_text(welcomed)
     raise StopPropagation
 
 @Bot.on_message(filters.private & filters.command("broadcast"))
@@ -71,35 +68,5 @@ async def sts(c, m):
         quote=True
     )
 
-
-@Bot.on_callback_query()
-async def callback_handlers(bot: Client, cb: CallbackQuery):
-    user_id = cb.from_user.id
-    if cb.data == "notifon":
-        notif = await db.get_notif(cb.from_user.id)
-        if notif is True:
-            await db.set_notif(user_id, notif=False)
-        else:
-            await db.set_notif(user_id, notif=True)
-        await cb.message.edit(
-            f"`Here You Can Set Your Settings:`\n\nSuccessfully setted notifications to **{await db.get_notif(user_id)}**",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            f"NOTIFICATION  {'🔔' if ((await db.get_notif(user_id)) is True) else '🔕'}",
-                            callback_data="notifon",
-                        )
-                    ],
-                    [InlineKeyboardButton("❎", callback_data="closeMeh")],
-                ]
-            ),
-        )
-        await cb.answer(
-            f"Successfully setted notifications to {await db.get_notif(user_id)}"
-        )
-    else:
-        await cb.message.delete(True)
-
-
+log.info("Bot Started - %s", bot_username)
 Bot.run()
