84d26047ebf29588ccb85bf1cbc009f02e00a4e9
Tushar Patil <86295965+PrinceStarLord@users.noreply.github.com>
2023-02-02 09:39:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,26 @@ Bot = Client(
 async def _(bot, cmd):
     await handle_user_status(bot, cmd)
 
+@Bot.on_message(filters.command("start") & filters.private)
+async def startprivate(client, message):
+    # return
+    chat_id = message.from_user.id
+    if not await db.is_user_exist(chat_id):
+        data = await client.get_me()
+        BOT_USERNAME = data.username
+        await db.add_user(chat_id)
+        if LOG_CHANNEL:
+            await client.send_message(
+                LOG_CHANNEL,
+                f"#NEWUSER: \n\nNew User [{message.from_user.first_name}](tg://user?id={message.from_user.id}) started @{BOT_USERNAME} !!",
+            )
+        else:
+            logging.info(f"#NewUser :- Name : {message.from_user.first_name} ID : {message.from_user.id}")
+    
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\nI'm a simple Telegram bot that can broadcast messages and media to the bot subscribers"
+    await message.reply_text(welcomed)
+    raise StopPropagation
+
 @Bot.on_message(filters.private & filters.command("broadcast"))
 async def broadcast_handler_open(_, m):
     if m.from_user.id not in AUTH_USERS:
@@ -47,38 +67,39 @@ async def sts(c, m):
         await m.delete()
         return
     await m.reply_text(
-        text=f"**TOTAL USERS :** `{await db.total_users_count()}`\n\n**Total Users, Notification Enabled 🔔 :** `{await db.total_notif_users_count()}`",
+        text=f"**Total Users in Database 📂:** `{await db.total_users_count()}`\n\n**Total Users with Notification Enabled 🔔 :** `{await db.total_notif_users_count()}`",
         quote=True
     )
 
-# @Bot.on_callback_query()
-# async def callback_handlers(bot: Client, cb: CallbackQuery):
-#     user_id = cb.from_user.id
-#     if cb.data == "notifon":
-#         notif = await db.get_notif(cb.from_user.id)
-#         if notif is True:
-#             await db.set_notif(user_id, notif=False)
-#         else:
-#             await db.set_notif(user_id, notif=True)
-#         await cb.message.edit(
-#             f"`Here You Can Set Your Settings:`\n\nSuccessfully setted notifications to **{await db.get_notif(user_id)}**",
-#             reply_markup=InlineKeyboardMarkup(
-#                 [
-#                     [
-#                         InlineKeyboardButton(
-#                             f"NOTIFICATION  {'🔔' if ((await db.get_notif(user_id)) is True) else '🔕'}",
-#                             callback_data="notifon",
-#                         )
-#                     ],
-#                     [InlineKeyboardButton("❎", callback_data="closeMeh")],
-#                 ]
-#             ),
-#         )
-#         await cb.answer(
-#             f"Successfully setted notifications to {await db.get_notif(user_id)}"
-#         )
-#     else:
-#         await cb.message.delete(True)
+
+@Bot.on_callback_query()
+async def callback_handlers(bot: Client, cb: CallbackQuery):
+    user_id = cb.from_user.id
+    if cb.data == "notifon":
+        notif = await db.get_notif(cb.from_user.id)
+        if notif is True:
+            await db.set_notif(user_id, notif=False)
+        else:
+            await db.set_notif(user_id, notif=True)
+        await cb.message.edit(
+            f"`Here You Can Set Your Settings:`\n\nSuccessfully setted notifications to **{await db.get_notif(user_id)}**",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            f"NOTIFICATION  {'🔔' if ((await db.get_notif(user_id)) is True) else '🔕'}",
+                            callback_data="notifon",
+                        )
+                    ],
+                    [InlineKeyboardButton("❎", callback_data="closeMeh")],
+                ]
+            ),
+        )
+        await cb.answer(
+            f"Successfully setted notifications to {await db.get_notif(user_id)}"
+        )
+    else:
+        await cb.message.delete(True)
 
 
 Bot.run()
