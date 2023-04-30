8e9908a511427ac3c5d3ee8723d58399da53d0d6
Tushar Patil <86295965+PrinceStarLord@users.noreply.github.com>
2023-04-30 12:37:31 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5868416665:AAHAkB-3pk4UO1UVtPsee5386ou5n_9pq4U")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6287552197:AAGvih00V0cGgf7iMuVJsijjE7YyMpGx3KE")
 API_ID = int(os.environ.get("API_ID", "5291894"))
 API_HASH = os.environ.get("API_HASH", "e15ad5a64cb8bd39b2be463d0314295f")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001875287218"))
-AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1916526576").split())
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001806709247"))
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1132901778").split())
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://renamev3-1:renamev3-1@cluster0.hyv9toa.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DB_NAME", "indurlBroadcastBot")
-BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
+DB_NAME = os.environ.get("DB_NAME", "TestBot")
+BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
